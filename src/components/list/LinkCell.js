40a6526e1e1fed55bfcb868afb4439887b036895
zhangrui <zhangrui@ys.com>
2017-07-05 16:37:16 +0800

    change

--- src/components/list/LinkCell.js
+++ src/components/list/LinkCell.js
@@ -1,11 +1,20 @@
 import React, { Component, PropTypes } from 'react'
+import {getPathVal} from '../../utils/data'
 
 class LinkCell extends Component {
-
     render(){
-        const {id,width,classes,links} = this.props;
-        const children = links.map(function(link,i){
-            return <a key={id+'_link_'+i} data-key={link.key} href={link.url}>{link.name}</a>;
+        const {width,classes,links,data,dataKey,maxLength} = this.props;
+        const value = getPathVal(data,dataKey);
+        let text = value;
+        let linkName = '';
+        if(maxLength!=null && text.length>maxLength){
+            text = text.substring(0,maxLength)+'...';
+        }
+        if(text == 0){
+            linkName = '中止';
+        }
+        const children = links.map(function(i){
+            return <a title={text}>{linkName}</a>;
         });
         return (
             <td width={width} className={classes}>
@@ -19,7 +28,10 @@ class LinkCell extends Component {
 LinkCell.propTypes = {
     width: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),
     classes: PropTypes.oneOfType([PropTypes.object,PropTypes.string]),
-    links: PropTypes.array
+    links: PropTypes.array,
+    data: PropTypes.object,
+    maxLength: PropTypes.number,
+    dataKey: PropTypes.string
 };
 
 export default LinkCell;
--- src/config/list/archive.js
+++ src/config/list/archive.js
@@ -6,11 +6,11 @@ const option = {
     limit:15,
     columns:[
         {label:'序号',cell:'IndexCell',width:40},
-        {label:'卷宗名称',cell:'ViewEntCell',links:[{url:'#',key:'detail'}],idKey:"id",dataKey:"name",width:230},
+        {label:'卷宗名称',cell:'ViewEntCell',idKey:"id",dataKey:"name",width:230},
         {label:'卷宗类型',dataKey:"type.name"},
         {label:'卷宗状态',dataKey:"state"},
         {label:'立卷时间',cell:'DateCell',type:'date',dataKey:"createTime"},
-        {label:'操作',cell:'LinkCell',links:[{name:'中止',url:'api/archive/list.json',key:'asssxx'}]}
+        {label:'操作',cell:'LinkCell',dataKey:"canPause",links:[{key:'asssxx'}]}
     ],
     url:'api/archive/list.json'
 };
