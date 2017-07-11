69fdecee62464b562d06356ea0b686090a54a95d
zhangrui <zhangrui@ys.com>
2017-07-11 20:50:56 +0800

    update

---
+++ src/reducers/protocolUpdate.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Admin on 2017/7/11.
+ */
+import  {PROTOCOL_UPDATE} from '../constants/ActionTypes'
+import syncReducer from './syncReducer'
+
+export default syncReducer({
+    [PROTOCOL_UPDATE]:{ }
+})
