dfd0f9dbff1fb94bcbc617458c0507a3513b80eb
wxdong5211 <wxdong5211@gmail.com>
2017-07-11 15:01:01 +0800

    add upload params

--- src/components/archive/UpLoading.js
+++ src/components/archive/UpLoading.js
@@ -1,16 +1,41 @@
-/**
- * Created by Administrator on 2017/7/7 0007.
- */
 import React, { Component, PropTypes } from 'react'
 import { Upload, Button, Icon } from 'antd';
 
 const fileList = [];
 
+function beforeUpload(file) {
+    const isJPG = true;
+    // const isJPG = file.type === 'image/jpeg';
+    // if (!isJPG) {
+    //     console.log('You can only upload JPG file!');
+    // }
+    console.log('upload type',file.type);
+    console.log('upload size',file.size);
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+        console.log('Image must smaller than 2MB!');
+    }
+    return isJPG && isLt2M;
+}
+
 const props = {
+    data:{aid:'xc'},
     action: '//jsonplaceholder.typicode.com/posts/',
     listType: 'picture',
     defaultFileList: [...fileList],
     className: 'upload-list-inline',
+    withCredentials: true,
+    beforeUpload: beforeUpload,
+    onChange(info) {
+        if (info.file.status !== 'uploading') {
+            console.log('info',info);
+        }
+        if (info.file.status === 'done') {
+            console.log(`${info.file.name} file uploaded successfully`);
+        } else if (info.file.status === 'error') {
+            console.log(`${info.file.name} file upload failed.`);
+        }
+    }
 };
 
 class UpLoading extends Component {
