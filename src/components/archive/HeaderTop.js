dbdbc619e1e2b819707bd1877ba9402dc3e27dde
zhangrui <zhangrui@ys.com>
2017-07-20 14:06:42 +0800

    style

--- src/components/archive/HeaderTop.js
+++ src/components/archive/HeaderTop.js
@@ -10,24 +10,25 @@ class HeaderTop extends Component {
     render() {
         const {data,isActive,add} = this.props;
         let style = '';
+        let numStyle = '';
         if(isActive){
             style = 'header-top tap-blue';
-            if(add =="add"){
-                style = 'header-top-gary tap-blue';
-            }
+            numStyle = 'num-click-blue'
         }else{
             if(add =="add"){
                 style = 'header-top-gary';
+                numStyle = 'num-gray'
             }else{
                 style = 'header-top';
+                numStyle = 'num-blue'
             }
         }
         return (
             <div
                 className={style}
-                data-route={data.route}
                 data-index={data.index}>
-                {data.name}
+                <div className={numStyle}>0{data.index+1}</div>
+                <div className="data-name" data-route={data.route}>{data.name}</div>
             </div>
         )
     }
