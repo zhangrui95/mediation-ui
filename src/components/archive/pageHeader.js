081788fec31527e63a1c1dd20713637009e9373c
zhangrui <zhangrui@ys.com>
2017-08-04 13:28:33 +0800

    change

--- src/components/archive/pageHeader.js
+++ src/components/archive/pageHeader.js
@@ -4,7 +4,7 @@ import {IMG_LOGO_URL} from '../../constants/Constant';
 class PageHeader extends Component {
     render() {
         return (
-                <div className="print-header-box">
+                <div className="print-header-box" id="print">
                     <div className="border-header"></div>
                     <div className="word-title-box">
                         <div className="font-big">多一分<span className="large-font">谦让</span>和<span className="large-font">理解</span></div>
